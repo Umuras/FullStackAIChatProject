8f142dff27f3e428fc6c1b6138997fd70a6d1767
Ali Umur Kucur <aliumurkucur@gmail.com>
2025-11-20 06:16:33 +0300

    frontend side changed.

--- frontend/chataiweb/src/Pages/RegisterPage.jsx
+++ frontend/chataiweb/src/Pages/RegisterPage.jsx
@@ -21,7 +21,7 @@ export function RegisterPage() {
   async function submitForm(formData) {
     try {
       const response = await axios
-        .post("http://localhost:5239/api/Auth/register", formData)
+        .post(`${API_BASE_URL}/api/Auth/register`, formData)
         .then((response) => {
           console.log("Kayıt Başarılı:", response.data);
           toast.success(
