33921e1c23c8085a849a343dc00f84a2d0416aaf
Ali Umur Kucur <aliumurkucur@gmail.com>
2025-11-20 04:50:00 +0300

    some changes made.

--- backend/Program.cs
+++ backend/Program.cs
@@ -15,7 +15,7 @@ var environment = builder.Environment;
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
     .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
     .AddEnvironmentVariables();
 
